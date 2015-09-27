1d6939e144d865773f262949e784e9c50b617426
thais <thaisbarras@hotmail.com>
2015-09-27 11:22:12 -0300

    Inserido url.py em pericia.app e diretorios html

--- pericia/pericia/urls.py
+++ pericia/pericia/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'pericia.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    #url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('periciaapp.urls')),
+
 ]
